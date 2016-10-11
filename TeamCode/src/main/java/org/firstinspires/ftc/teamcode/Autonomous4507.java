40d35e762c91c51b5229db89130814c6a065a23f
CCHS RoboChargers <cchsrobochargers@gmail.com>
2016-10-11 19:42:58 -0400

    Auto in the works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous4507.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous4507.java
@@ -26,12 +26,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 
 /**
@@ -39,25 +41,28 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @Autonomous(name="Autonomous", group="4507")  // @TeleOp(...) is the other common choice
-@Disabled
+//@Disabled
 public class Autonomous4507 extends LinearOpMode {
 
     // DcMotorController
-    DcMotorController dtCtlr;
+    DcMotor l1;
+    DcMotor l2;
+    DcMotor r1;
+    DcMotor r2;
     // Servos
-    Servo beaconPinion;
-    Servo beaconPusher;
-    // Sensors
+//    Servo beaconPinion;
+//    Servo beaconPusher;
+//    // Sensors
     GyroSensor gyro;
-    ColorSensor bColor;
-    ColorSensor fColor;
-    UltrasonicSensor beaconU;
-    //Switches
-    DigitalChannel tileSw;
-    DigitalChannel colorSw;
-    DigitalChannel beaconSw;
-    // Elapsed Time
-    ElapsedTime elapsedTime;
+//    ColorSensor bColor;
+//    ColorSensor fColor;
+//    UltrasonicSensor beaconU;
+//    //Switches
+//    DigitalChannel tileSw;
+//    DigitalChannel colorSw;
+//    DigitalChannel beaconSw;
+//    // Elapsed Time
+//    ElapsedTime elapsedTime;
 
     // Global State Vaiables
     double countsPerYard = 30000.0;
@@ -67,6 +72,10 @@ public class Autonomous4507 extends LinearOpMode {
     double slowSpeed;
     long fastSpDelay;
     long slowSpDelay;
+    int speedToCounts[] = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5,
+        5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9};
 
     // These variables are for autonomous.
     boolean red;
@@ -104,47 +113,51 @@ public class Autonomous4507 extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // DcMotorControllers - 1 = left
-        dtCtlr = hardwareMap.dcMotorController.get("dtCtlr");
-        dtCtlr.setMotorMode(1, DcMotor.RunMode.RUN_TO_POSITION);
-        dtCtlr.setMotorMode(2, DcMotor.RunMode.RUN_TO_POSITION);
+        // DcMotorControllers
+        l1 = hardwareMap.dcMotor.get("l1");
+//        l1.setDirection(DcMotorSimple.Direction.REVERSE);
+        l2 = hardwareMap.dcMotor.get("l2");
+        r1 = hardwareMap.dcMotor.get("r1");
+        r2 = hardwareMap.dcMotor.get("r2");
+//        r2.setDirection(DcMotorSimple.Direction.REVERSE);
         // Servos
-        beaconPinion = hardwareMap.servo.get("bPi");
-        beaconPusher = hardwareMap.servo.get("bPu");
-        beaconPinion.setPosition(0.5);
-        beaconPusher.setPosition(0.5);
-        // Sensors
+//        beaconPinion = hardwareMap.servo.get("bPi");
+//        beaconPusher = hardwareMap.servo.get("bPu");
+//        beaconPinion.setPosition(0.5);
+//        beaconPusher.setPosition(0.5);
+//        // Sensors
         gyro = hardwareMap.gyroSensor.get("gyro");
-        bColor = hardwareMap.colorSensor.get("bC");
-        fColor = hardwareMap.colorSensor.get("fC");
-        beaconU = hardwareMap.ultrasonicSensor.get("bU");
-        //Switches
-        tileSw = hardwareMap.digitalChannel.get("tSw");
-        colorSw = hardwareMap.digitalChannel.get("cSw");
-        beaconSw = hardwareMap.digitalChannel.get("bSw");
-        if(tileSw.getState()) {
-            diagTile = true;
-            straightTile = false;
-        } else if (!tileSw.getState()) {
-            diagTile = false;
-            straightTile = true;
-        }
-        if(colorSw.getState()) {
-            red = true;
-            blue = false;
-        } else if (!colorSw.getState()) {
-            red = false;
-            blue = true;
-        }
-        if (beaconSw.getState()) {
-            beaconY = true;
-            beaconN = true;
-        } else if (!beaconSw.getState()) {
-            beaconY = false;
-            beaconN = true;
-        }
-        // Elapsed Time
-        elapsedTime = new ElapsedTime();
+//        bColor = hardwareMap.colorSensor.get("bC");
+//        fColor = hardwareMap.colorSensor.get("fC");
+//        beaconU = hardwareMap.ultrasonicSensor.get("bU");
+//        //Switches
+//        tileSw = hardwareMap.digitalChannel.get("tSw");
+//        colorSw = hardwareMap.digitalChannel.get("cSw");
+//        beaconSw = hardwareMap.digitalChannel.get("bSw");
+//        if(tileSw.getState()) {
+//            diagTile = true;
+//            straightTile = false;
+//        } else if (!tileSw.getState()) {
+//            diagTile = false;
+//            straightTile = true;
+//        }
+//        if(colorSw.getState()) {
+//            red = true;
+//            blue = false;
+//        } else if (!colorSw.getState()) {
+//            red = false;
+//            blue = true;
+//        }
+//        if (beaconSw.getState()) {
+//            beaconY = true;
+//            beaconN = true;
+//        } else if (!beaconSw.getState()) {
+//            beaconY = false;
+//            beaconN = true;
+//        }
+//        // Elapsed Time
+//        elapsedTime = new ElapsedTime();
+        gyro.calibrate();
 
         // Wait for the robot to start
         waitForStart();
@@ -169,29 +182,39 @@ public class Autonomous4507 extends LinearOpMode {
          *
          */
 
-        if (straightTile) {
-            driveStraight(move1a + move1b, fastSpeed, fastSpDelay);
-        } else if (diagTile) {
-            driveStraight(move0a, fastSpeed, fastSpDelay);
-            driveTurn(turn0a, mediumSpeed, slowSpDelay);
-            driveStraight(move0b, fastSpeed, fastSpDelay);
-            driveTurn(turn0b, mediumSpeed, slowSpDelay);
-            driveStraight(move1b, fastSpeed, fastSpDelay);
-        }
-        if (beaconY) {
-            driveTurn(turn1, mediumSpeed, slowSpDelay);
-            driveStraight(move2, fastSpeed, fastSpDelay);
-            driveTurn(turn2, mediumSpeed, slowSpDelay);
-            driveStraight(move3, slowSpeed,slowSpDelay);
-            // add button code
-            driveStraight(move4a, fastSpeed, fastSpDelay);
-            driveStraight(move4b, slowSpeed, slowSpDelay);
-            // add button code
-            driveStraight(move5, fastSpeed, fastSpDelay);
-            driveTurn(turn3, mediumSpeed, slowSpDelay);
-            driveStraight(move6, fastSpeed, fastSpDelay);
-        }
+//        if (straightTile) {
+//            driveStraight(move1a + move1b, fastSpeed, fastSpDelay);
+//        } else if (diagTile) {
+//            driveStraight(move0a, fastSpeed, fastSpDelay);
+//            driveTurn(turn0a, mediumSpeed, slowSpDelay);
+//            driveStraight(move0b, fastSpeed, fastSpDelay);
+//            driveTurn(turn0b, mediumSpeed, slowSpDelay);
+//            driveStraight(move1b, fastSpeed, fastSpDelay);
+//        }
+//        if (beaconY) {
+//            driveTurn(turn1, mediumSpeed, slowSpDelay);
+//            driveStraight(move2, fastSpeed, fastSpDelay);
+//            driveTurn(turn2, mediumSpeed, slowSpDelay);
+//            driveStraight(move3, slowSpeed,slowSpDelay);
+//            // add button code
+//            driveStraight(move4a, fastSpeed, fastSpDelay);
+//            driveStraight(move4b, slowSpeed, slowSpDelay);
+//            // add button code
+//            driveStraight(move5, fastSpeed, fastSpDelay);
+//            driveTurn(turn3, mediumSpeed, slowSpDelay);
+//            driveStraight(move6, fastSpeed, fastSpDelay);
+//        }
+        driveStraight(12, 1.0, 5000);
+        driveTurn(90, 5000);
+        driveStraight(12, 1.0, 5000);
+
+
+
+
+
+
     }
+
 
     /**
      * Created on 9/10/16
@@ -205,26 +228,14 @@ public class Autonomous4507 extends LinearOpMode {
         int lTarget;
         int rTarget;
 
-        lTarget = dtCtlr.getMotorCurrentPosition(1) + (int)(inches * (countsPerYard / 36.0));
-        rTarget = dtCtlr.getMotorCurrentPosition(2) + (int)(inches * (countsPerYard / 36.0));
+        lTarget = l1.getCurrentPosition() + (int)(inches * (countsPerYard / 36.0));
+        rTarget = r1.getCurrentPosition() + (int)(inches * (countsPerYard / 36.0));
 
-
-
-//        lDrive.setPower(speed);
-//        rDrive.setPower(speed);
-//
-//        while (!lDrive.isBusy() || !rDrive.isBusy()) {
-//            // Empty While loop runs until motors start
-//        }
-//
-//        while (lDrive.isBusy() || rDrive.isBusy()) {
-//            // Empty While loop runs until motors stop
-//        }
-
+        setMotorPosition(lTarget, rTarget, speed);
         sleep(delayMillis);
     }
 
-    public void driveTurn (int degrees, double speed, long delay) throws InterruptedException {
+    public void driveTurn (int degrees, long delay) throws InterruptedException {
         int currentHeading;
         int startHeading;
         int desiredHeading;
@@ -235,11 +246,14 @@ public class Autonomous4507 extends LinearOpMode {
             degrees = degrees * -1;
         }
 
-//        lDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        desiredHeading = gyro.getHeading();
         startHeading = gyro.getHeading();
+        desiredHeading = gyro.getHeading() + degrees;
+        if (desiredHeading > 360) {
+            desiredHeading = desiredHeading - 360;
+        }
+        if (desiredHeading < 0) {
+            desiredHeading = desiredHeading + 360;
+        }
         gyroError = desiredHeading - gyro.getHeading();
         if (gyroError > 180) {
             gyroError = 360 - gyroError;
@@ -248,13 +262,7 @@ public class Autonomous4507 extends LinearOpMode {
             gyroError = 360 + gyroError;
         }
 
-        desiredHeading = gyro.getHeading() + degrees;
-        if (desiredHeading > 360) {
-            desiredHeading = desiredHeading - 360;
-        }
-        if (desiredHeading < 0) {
-            desiredHeading = desiredHeading + 360;
-        }
+
 
         while (!exitTurn) {
             gyroError = gyro.getHeading() - desiredHeading;
@@ -267,9 +275,13 @@ public class Autonomous4507 extends LinearOpMode {
             if (gyroError == 0) {
                 exitTurn = true;
             }
+            l1.setPower(Range.clip(0 - (gyroError * 0.05), -1.0, 1.0));
+            l2.setPower(Range.clip(0 - (gyroError * 0.05), -1.0, 1.0));
+            r1.setPower(Range.clip(0 + (gyroError * 0.05), -1.0, 1.0));
+            r2.setPower(Range.clip(0 + (gyroError * 0.05), -1.0, 1.0));
             idle();
+            sleep(delay);
         }
-        sleep(delay);
     }
 
     /**
@@ -282,43 +294,45 @@ public class Autonomous4507 extends LinearOpMode {
 
     }
 
-    public void pushButton () {
-        while (beaconU.getUltrasonicLevel() > beaconDist) {
-            beaconPinion.setPosition(1.0);
-        }
+//    public void pushButton () {
+//        while (beaconU.getUltrasonicLevel() > beaconDist) {
+//            beaconPinion.setPosition(1.0);
+//        }
+//
+//    }
 
-    }
-
-    public void setMotorPosition (int port, int lTarget, int rTarget, double speed) throws InterruptedException {
-        long preTime;
-        int preEncoderPos = dtCtlr.getMotorCurrentPosition(1);
+    public void setMotorPosition (int lTarget, int rTarget, double speed) throws InterruptedException {
+        int preEncoderPos = l1.getCurrentPosition();
+        int b4EncoderPos;
         int speedUpDist;
-        dtCtlr.setMotorTargetPosition(1, lTarget);
-        dtCtlr.setMotorTargetPosition(2, rTarget);
-
-        for(int c = 0; c < speed * 100; c++) {
-            dtCtlr.setMotorPower(1, (double)c / 100.0);
-            dtCtlr.setMotorPower(2, (double)c / 100.0);
-            preTime = System.currentTimeMillis();
-            while (preTime + 1 < System.currentTimeMillis() || endMove) {
+        int direction = 1;
+        if (speed < 0) {
+            direction = -1;
+        }
+        for(int c = 0; c < (int)speed * 100; c++) {
+            b4EncoderPos = l1.getCurrentPosition();
+            l1.setPower((double)c / 100.0 * direction);
+            l2.setPower((double)c / 100.0 * direction);
+            r1.setPower((double)c / 100.0 * direction);
+            r2.setPower((double)c / 100.0 * direction);
+            while (l1.getCurrentPosition() < b4EncoderPos + speedToCounts[c]) {
                 idle();
             }
         }
-        speedUpDist = dtCtlr.getMotorCurrentPosition(1) - preEncoderPos;
-        while (dtCtlr.getMotorTargetPosition(1) - speedUpDist > dtCtlr.getMotorCurrentPosition(1) || endMove) {
+        speedUpDist = l1.getCurrentPosition() - preEncoderPos;
+        while (lTarget - speedUpDist > l1.getCurrentPosition()) {
             idle();
         }
         for(int c = (int)speed * 100; c <= 0; c--) {
-            dtCtlr.setMotorPower(1, (double)c / 100.0);
-            dtCtlr.setMotorPower(2, (double)c / 100.0);
-            preTime = System.currentTimeMillis();
-            while (preTime + 1 < System.currentTimeMillis() || endMove) {
+            b4EncoderPos = l1.getCurrentPosition();
+            l1.setPower((double)c / 100.0 * direction);
+            l2.setPower((double)c / 100.0 * direction);
+            r1.setPower((double)c / 100.0 * direction);
+            r2.setPower((double)c / 100.0 * direction);
+            while (l1.getCurrentPosition() < b4EncoderPos + speedToCounts[c]) {
                 idle();
             }
         }
-
-
-
     }
 
 }
