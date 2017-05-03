16cbece72467c06acb65a4489177f87ff67049e5
RoboChargers <cchsrobochargers@gmail.com>
2017-05-02 21:58:06 -0400

    Wheel for henry going well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBabyGo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBabyGo.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.internal.AppUtil;
 
 @TeleOp(name = "Go Baby Go!")
 public class GoBabyGo extends OpMode {
@@ -18,9 +22,15 @@ public class GoBabyGo extends OpMode {
     TouchSensor leftSteerStop;
     TouchSensor rightSteerStop;
 
+    AnalogInput xAxis;
+    AnalogInput yAxis;
+    double xMax;
+    double yMax;
+
     SpeedMonitor monitor;
 
     boolean active = false;
+    boolean override = false;
 
     public void init() {
         leftDrive = hardwareMap.dcMotor.get("lD");
@@ -31,26 +41,51 @@ public class GoBabyGo extends OpMode {
         leftSteerStop = hardwareMap.touchSensor.get("lT");
         rightSteerStop = hardwareMap.touchSensor.get("rT");
 
+        xAxis = hardwareMap.analogInput.get("x");
+        yAxis = hardwareMap.analogInput.get("y");
+        xMax = xAxis.getMaxVoltage();
+        yMax = yAxis.getMaxVoltage();
+
         active = true;
         monitor = new SpeedMonitor(leftDrive, rightDrive, 3000);
         monitor.start();
     }
 
     public void loop() {
-        double speed = -gamepad1.right_stick_y;
-        double steer = gamepad1.right_stick_x;
+        double speed;
+        double steer;
+        if (override) {
+            speed = -gamepad1.right_stick_y;
+            steer = gamepad1.right_stick_x;
+            AppUtil.getInstance().showToast(("User Override!" + '\n' + "Press " + '\'' + 'a' + '\'' + " to give control back to Henry"), hardwareMap.appContext, Toast.LENGTH_SHORT);
+
+            if (gamepad1.a) {
+                override = false;
+            }
+        } else {
+            if (!gamepad1.atRest()) {
+                override = true;
+            }
+            speed = Range.scale(yAxis.getVoltage() / yMax, 0.0, 1.0, -1.0, 1.0);
+            speed *= -1;
+            steer = Range.scale(xAxis.getVoltage() / xMax, 0.0, 1.0, -1.0, 1.0);
+            AppUtil.getInstance().showToast(("Henry's in control" + '\n' + "Touch the joysticks to override his control"), hardwareMap.appContext, Toast.LENGTH_SHORT);
+        }
 
         speed = Range.clip(speed, -1.0, 1.0);
         if (rightSteerStop.isPressed()) {
             steer = Range.clip(steer, -1.0, 0.0);
         } else if (leftSteerStop.isPressed()) {
             steer = Range.clip(steer, 0.0, 1.0);
+        } else if (leftSteerStop.isPressed() && rightSteerStop.isPressed()) {
+            steer = 0.0;
         } else {
             steer = Range.clip(steer, -1.0, 1.0);
         }
 
         monitor.setPower(speed);
         this.steer.setPower(steer);
+
 
     }
 
